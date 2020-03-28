02502233898231217432adbd04533a78ce0064df
roberfuentes <roberfuentes@users.noreply.github.com>
2020-03-28 14:09:34 +0100

    Login logic and redirects automatically to MainActivity

--- FinalProjectApplication/app/src/main/java/com/example/finalprojectapplication/Activities/Login/LoginActivity.java
+++ FinalProjectApplication/app/src/main/java/com/example/finalprojectapplication/Activities/Login/LoginActivity.java
@@ -1,23 +1,34 @@
 package com.example.finalprojectapplication.Activities.Login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.finalprojectapplication.Activities.Main.MainActivity;
 import com.example.finalprojectapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity
 {
 
+    private String TAG;
     private FirebaseAuth mAuth;
 
+    EditText mEtEmail, mEtPassword;
+
     Button mBtnRegisterEmail;
+    Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,13 +36,31 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Variables
+        TAG = "LoginActivity";
         //Firebase
         mAuth = FirebaseAuth.getInstance();
 
 
         //Views
+        mEtEmail = findViewById(R.id.etEmail);
+        mEtPassword = findViewById(R.id.etPassword);
+
+        mBtnLogin = findViewById(R.id.btnLogin);
         mBtnRegisterEmail = findViewById(R.id.btnRegisterEmail);
 
+
+        //BUTTON ON CLICK LISTENERS
+
+        //Sign in
+        mBtnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                login();
+            }
+        });
 
 
 
@@ -44,28 +73,59 @@ public class LoginActivity extends AppCompatActivity
                 goToRegisterActivity();
             }
         });
-
-
     }
 
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
 
-        FirebaseUser current = mAuth.getCurrentUser();
-        if(current == null){
-            Toast.makeText(this, "There's no user my G", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, current.getUid(), Toast.LENGTH_SHORT).show();
+
+    private void login(){
+        String email = mEtEmail.getText().toString();
+        String password = mEtPassword.getText().toString();
+
+        if(checkFields(email, password)){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+                        goToMainActivity();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Credentials are not valid!", Toast.LENGTH_SHORT).show();
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    }
+                }
+            });
         }
 
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT);
     }
 
-    protected void goToRegisterActivity(){
+
+    //INTENTS
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToRegisterActivity(){
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
+
+
+
+
+
+    private boolean checkFields(String email, String password){
+        if(!email.equals("") && !password.equals("")){
+            return true;
+        }else{
+            Toast.makeText(LoginActivity.this, "Fields can't be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    }
+
 }
